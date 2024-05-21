42afd84bc2d6802714c36d5185b373ebf1b2191c
Serhii Kolesnyk <110739720+FlowMaster83@users.noreply.github.com>
2024-05-21 17:58:24 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
+import OfficerList from "./OfficerList";
+import data from "../officers.json";
+
 export default function App() {
-    return (
-        <>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore sit voluptates ipsa eius molestiae repudiandae accusantium. Ullam aliquid quod temporibus. Obcaecati sapiente quo molestiae aperiam in beatae tempore quasi aliquam?</p>
-        </>
-    )
+  return (
+    <>
+      <h1>Available Officers</h1>
+      <OfficerList officers={data} />
+    </>
+  );
 }
