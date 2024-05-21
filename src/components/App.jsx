629998aa44c12bc9006eb34834dea3562b5c69b9
Serhii Kolesnyk <110739720+FlowMaster83@users.noreply.github.com>
2024-05-21 17:36:03 +0300

    test

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore sit voluptates ipsa eius molestiae repudiandae accusantium. Ullam aliquid quod temporibus. Obcaecati sapiente quo molestiae aperiam in beatae tempore quasi aliquam?</p>
+        </>
+    )
+}
